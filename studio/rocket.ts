d5ce6e10b6733eb28cb8d6b79f36a3b3bcc6d86f
Hari Mahat <harimahat@Haris-MBP.lan>
2020-09-24 14:04:55 +0300

    Projects ready for submission

--- studio/rocket.ts
+++ studio/rocket.ts
@@ -1,1 +1,47 @@
-export class Rocket {}
+import { Astronaut } from "./astronaut";
+import { Cargo } from "./cargo";
+import { Payload } from "./Payload";
+
+export class Rocket implements Payload {
+  name: string;
+  totalCapacityKg: number;
+  cargoItems: Cargo[] = [];
+  astronauts: Astronaut[] = [];
+  massKg: number;
+
+  constructor(name: string, totalCapacityKg: number) {
+    this.name = name;
+    this.totalCapacityKg = totalCapacityKg;
+  }
+
+  sumMass(items: Payload[]): number {
+    let sum: number = 0;
+    items.forEach((item) => {
+      sum += item.massKg;
+    });
+    /*  for (let i = 0; i < items.length; i++) {
+      sum += items[i].massKg;
+    } */
+    return sum;
+  }
+  currentMassKg(): number {
+    return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+  }
+  canAdd(item: Payload): boolean {
+    return this.currentMassKg() + item.massKg <= this.totalCapacityKg;
+  }
+  addCargo(cargo: Cargo): boolean {
+    if (this.canAdd) {
+      this.cargoItems.push(cargo);
+      return true;
+    }
+    return false;
+  }
+  addAstronaut(astronaut: Astronaut): boolean {
+    if (this.canAdd) {
+      this.astronauts.push(astronaut);
+      return true;
+    }
+    return false;
+  }
+}
